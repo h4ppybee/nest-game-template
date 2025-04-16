cad04ebad552b77f9342a5ad1c359f553def63cc
suyeon <suyeon@doubleugames.com>
2025-04-16 18:55:38 +0900

    feat: add serve package and web start script, update server port to 4000

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new PacketLoggingInterceptor());
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 4000;
   await app.listen(port);
 }
 void bootstrap();
